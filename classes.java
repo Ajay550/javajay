176b67f4edbe55147c1b37f3854abb8860c493da
Ajay55 <30935285+Ajay55@users.noreply.github.com>
2018-03-24 05:33:36 +0530

    Create classes.java

---
+++ classes.java
@@ -0,0 +1,23 @@
+class demo {
+	demo(){
+		System.out.println("hello ajay");
+	}
+	void show()
+	{
+		System.out.println("bye");
+	}
+	static{
+		System.out.println("aj");
+	}
+	
+	
+
+}
+
+class hello{
+          static public void main(String[]args)
+		  {
+		    demo a1= new demo();
+			  a1.show();
+		  }
+}
